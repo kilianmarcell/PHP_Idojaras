e4b52f91e819d1412341fa18c130f3096bac8609
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-10-20 11:24:53 +0200

    Példa

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ if ($bejegyzesId === null) {
     header('Location: index.php'); //átirányítás
     exit(); //leállítja a programot
 }
+
+$bejegyzesId = Bejegyzes::getById($bejegyzesId);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $ujTartalom = $_POST['tartalom'] ?? '';
+    $bejezes -> setTartalom($ujTartalom);
+    // UPDATE...
+    $bejezes -> mentes();
+}
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +24,7 @@ if ($bejegyzesId === null) {
     <title>Időjárás</title>
 </head>
 <body>
-    
+    <form method="POST"></form>
+    <input type="text" name="" id="">
 </body>
 </html>
